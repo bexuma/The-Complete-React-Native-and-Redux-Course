24a10f66a8ec4b550d40c57e4349654d065ee041
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-09-10 15:21:28 +0600

    emailChanged, passwordChanged actions implemented and added to LoginForm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import firebase from '@firebase/app';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  componentWillMount() {
+    this.initializeFirebase();
+  }
+
+  initializeFirebase() {
+    const config = {
+      apiKey: 'AIzaSyAtVRCe4cEDSvjZ9f6tlUWUMbZlvKPpTQI',
+      authDomain: 'rn-udemy-course.firebaseapp.com',
+      databaseURL: 'https://rn-udemy-course.firebaseio.com',
+      projectId: 'rn-udemy-course',
+      storageBucket: 'rn-udemy-course.appspot.com',
+      messagingSenderId: '827867252444'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>Hello</Text>
-        </View>
+        <LoginForm/>
       </Provider>  
     )
   }
