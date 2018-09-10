0717d6753205852f035b242b4a50b255e3202a2e
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2018-09-10 16:22:42 +0600

    Downgraded firebase to 5.0.2, added user to state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import firebase from '@firebase/app';
+import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
